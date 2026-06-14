9a21c43fd7550f175f52e31f51d6289848d0835a
GODWIN VUDU <adoniscrowe@gmail.com>
2026-06-14 22:00:04 +0000

    Add files via upload

---
+++ password.py
@@ -0,0 +1,12 @@
+#i need to make a password but how
+#using loops and coditionals statements
+def main():
+    name=input("whats your name: ")
+    password=input("enter your password : ")
+    x=("then")
+    while password !=x:
+            password=input("password not correct,enter right password : ")
+    print(f"welcome {name}")
+            
+main()
+
