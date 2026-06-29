fc9c8f6016ca2c33ca5d064ce03b54da3794b15d
Godwin Vudu <adoniscrowe@gmail.com>
2026-06-29 00:34:55 +0000

    again

--- and_or_not_and_loops.py
+++ and_or_not_and_loops.py
@@ -8,3 +8,13 @@ legal=(y==0 or x/1==6)
 print(legal)
 #loops
 #while loops
+#as long as a condition remains true,excutre the statements
+def main():
+    count=500
+    message="i am"
+    i=0
+    while i< count:
+        print(i,message)
+        i+=1
+#main()
+#i want to execute other commands
