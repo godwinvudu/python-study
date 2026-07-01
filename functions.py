7d9c86ab389e93952f43020fe657d83eb45bfe73
Godwin Vudu <adoniscrowe@gmail.com>
2026-07-01 21:42:34 +0000

    well

--- functions.py
+++ functions.py
@@ -8,19 +8,21 @@ print(sum(2, 3))
 #ask user for name
 def username():
     user_name=input("enter username:" )
-    want_to_play=input(f"hey {user_name} ready to play rock ,paper,sciscors")
-    while want_to_play:
+    while True:
+        want_to_play=input(f"hey {user_name} ready to play rock ,paper,sciscors").lower()
+    
         if want_to_play=="no" or "No":
             print("good bye then")
             break
         elif want_to_play=="yes" or "Yes":
             print("ok lets's do this")
+            break
         else:
           print("enter either a yes or no :")
        
     return username
 
 def main():
-    start_1=username
+    start_1=username()
 
 
