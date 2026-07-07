085de10e0a059f274a5b952601274562f7503372
Godwin Vudu <adoniscrowe@gmail.com>
2026-07-07 21:49:11 +0000

    Jul 7, 2026, 9:49 PM

--- python data_structures.py
+++ python data_structures.py
@@ -1,14 +1,1 @@
-#function parameter =variable representing input in the function definition
-def print_welcome(name):
-    print(f"welcome{name}!")
-username =(input("enter username:"))
-print (f"welcome {username}")
-a=9
-b=50
-for i in range(a,b,4):
-    print(i)
-def is_prime(n):
-    if n==2:
-        return True
-    elif n**0.5(n)==0:
-        return False
+#modules
