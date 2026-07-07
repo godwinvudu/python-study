8b46249c46a46f73c11f94c7c5cee6c6c9075578
Godwin Vudu <adoniscrowe@gmail.com>
2026-07-07 19:31:20 +0000

    learning more functions

---
+++ python data_structures.py
@@ -0,0 +1,9 @@
+#function parameter =variable representing input in the function definition
+def print_welcome(name):
+    print(f"welcome{name}!")
+username =(input("enter username:"))
+print (f"welcome {username}")
+a=9
+b=50
+for i in range(a,b,4):
+    print(i)
