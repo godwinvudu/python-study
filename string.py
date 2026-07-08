21ef6a34771411430f51781098b0c396246b4148
Godwin Vudu <adoniscrowe@gmail.com>
2026-07-08 22:44:59 +0000

    Jul 8, 2026, 10:44 PM

--- string.py
+++ string.py
@@ -34,13 +34,16 @@ print(q)
 #in in loops(for)
 for n in "string": 
   print(n, end = " ") 
-  break
 
-  count = 0 
+
+count = 0 
 for c in "abca": 
   if c == "a": 
     count += 1 
+  
 print(count)
+
+
 word = "cab" 
 for i in word: 
   if i == "a": 
@@ -49,7 +52,11 @@ for i in word:
     print("B", end = "") 
   if i == "c": 
     print("C", end = "") 
-  break
+
+
 
 #count()-counts number of occurences of a substring in a given string
 print("aaa".count("a"))
+
+#find() returns the indexc of the first occurence of asubstring
+print("banana".find("a"))
