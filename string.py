9f318fccf4b3828438278924ea439529bd6dc1f6
Godwin Vudu <adoniscrowe@gmail.com>
2026-07-08 22:02:18 +0000

    Jul 8, 2026, 10:02 PM

---
+++ string.py
@@ -0,0 +1,3 @@
+#strings= a sequence of characters
+#modifying strings :Using lower() returns converted alphabet characters to lowercase
+#upper() returns converted alphabetical characters to uppercase
