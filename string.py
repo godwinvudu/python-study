84d5f2219176ddf64faf8fc7547958da12ec101d
Godwin Vudu <adoniscrowe@gmail.com>
2026-07-08 22:16:12 +0000

    Jul 8, 2026, 10:16 PM

--- string.py
+++ string.py
@@ -11,3 +11,9 @@ def upper_1():
     checkingupper=input("enter something but should be in lower case:").upper()
     print(checkingupper)
 upper_1()
+#index slicing
+#when a programmer must get access to a sequence of characters
+
+time_string="12:18666"
+minutes=time_string[3:8]
+print(minutes)
