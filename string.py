1f6b97902f7c025d2d5c1faa36455b8757ac574d
Godwin Vudu <adoniscrowe@gmail.com>
2026-07-08 22:07:23 +0000

    Jul 8, 2026, 10:07 PM

--- string.py
+++ string.py
@@ -1,3 +1,13 @@
 #strings= a sequence of characters
 #modifying strings :Using lower() returns converted alphabet characters to lowercase
 #upper() returns converted alphabetical characters to uppercase
+#eg
+def lower1():
+    checkinglower=input("enter something capitalised:").lower()
+    print(checkinglower)
+lower1()
+
+def upper_1():
+    checkingupper=input("enter something but should be in lower case:").upper()
+    print(checkingupper)
+upper_1()
