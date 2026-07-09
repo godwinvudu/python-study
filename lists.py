5c153e444db107b98094a12fcea0e6e9a26ffa74
Godwin Vudu <adoniscrowe@gmail.com>
2026-07-09 00:21:00 +0000

    Jul 9, 2026, 12:21 AM

---
+++ lists.py
@@ -0,0 +1,8 @@
+#lists are useful ways of collecting data
+#lists content can be changed
+list_1=["hello","hi","howdy"]
+print(list_1)
+#modifying lists
+#an append()=adding an element to the end of the a lsi
+#remove()=removes the specified element from a lsit
+#pop()=removes last item from list
