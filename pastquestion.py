41cfe2652ac9761df486c8f86182bd06f5d49f76
Godwin Vudu <adoniscrowe@gmail.com>
2026-08-10 18:08:35 +0000

    Fix print statement for total rainfall in collect_rainfall function and add analyze_rainfall function

--- pastquestion.py
+++ pastquestion.py
@@ -17,5 +17,6 @@ def collect_rainfall():
     total=(day_1+day_2+day_3+day_4+day_5+day_6+day_7)
     return total
 result=collect_rainfall()
-print(f"Recorded amount: {result}")
+print(f"total rainfall: {result}")
+
 def analyze_rainfall():
