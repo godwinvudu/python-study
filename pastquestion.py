e32a0f71c6b6149efc1b76e43d20964cecca6b74
Godwin Vudu <adoniscrowe@gmail.com>
2026-08-10 17:02:31 +0000

    Aug 10, 2026, 5:02 PM

--- pastquestion.py
+++ pastquestion.py
@@ -2,3 +2,6 @@ def collect_rainfall():
     bootstrap = "Enter rainfall amount for"
     day_one = float(input(f"{bootstrap} day one: "))
     return day_one
+
+result=collect_rainfall()
+print(f"Recorded amount: {result}")
