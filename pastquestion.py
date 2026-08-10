6cf3055b3b2d9fad7cf9b05e2fad2f116f92da66
Godwin Vudu <adoniscrowe@gmail.com>
2026-08-10 16:51:18 +0000

    Fix variable naming and formatting in collect_rainfall function

--- pastquestion.py
+++ pastquestion.py
@@ -1,5 +1,5 @@
 def collect_rainfall():
     bootstrap=("Enter rainfall amount for")
-    Day_one=float(input(f"{bootstrap} day one"))
+    day_one=float(input(f"{bootstrap} day one"))
 
-    return(Day_one)
+    return day_one
