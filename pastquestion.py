a3932173eb7f03b768d30f30ede9097a0fe6e166
Godwin Vudu <adoniscrowe@gmail.com>
2026-08-10 17:11:17 +0000

    Aug 10, 2026, 5:11 PM

--- pastquestion.py
+++ pastquestion.py
@@ -3,10 +3,16 @@ def collect_rainfall():
     day_one = float(input(f"{bootstrap} day one: "))
 
     day_2=float(input(f"{bootstrap} day two: "))
+
     day_3=float(input(f"{bootstrap} day three: "))
+
     day_4=float(input(f"{bootstrap} day four: "))
+
     day_5=float(input(f"{bootstrap} day five: "))
+
     day_6=float(input(f"{bootstrap} day six: "))
+
     day_6=float(input(f"{bootstrap} day seven: "))
+    
 result=collect_rainfall()
 print(f"Recorded amount: {result}")
